825f8f1ed2fe2494ad1cdf39ccec316dd0e25429
TheAmniel <82575318+TheAmniel@users.noreply.github.com>
2024-06-22 17:08:24 -0400

    Added mutex-protected cache for translations, implemented file parsing and caching, and refactored I18n initialization and locale handling.

--- packages/i18n/src/lib.rs
+++ packages/i18n/src/lib.rs
@@ -10,14 +10,22 @@ use napi::{Error, Result, Status};
 use napi_derive::napi;
 use once_cell::sync::Lazy;
 
-use std::path;
+use std::{
+  path,
+  sync::{Mutex, MutexGuard},
+};
 
-use self::config::Config;
+use self::{
+  config::Config,
+  file::{parse, TObject, Translations},
+};
 
+static CACHE: Lazy<Mutex<Translations>> = Lazy::new(|| Mutex::new(Translations::new()));
 static BRACKETS_RE: Lazy<regex::Regex> = Lazy::new(|| regex::Regex::new(r"#\{([\w\.]+)\}").unwrap());
 static LOCALE_RE: Lazy<regex::Regex> = Lazy::new(|| regex::Regex::new(r"[a-z]{2,2}(\-|\_)[A-Z]{2,2}").unwrap());
 static LOCALE_STRICT_RE: Lazy<regex::Regex> =
   Lazy::new(|| regex::Regex::new(r"^[a-z]{2,2}(\-|\_)[A-Z]{2,2}$").unwrap());
+static FILENAME_RE: Lazy<regex::Regex> = Lazy::new(|| regex::Regex::new(r"^(.*?)\.[^.]+$").unwrap());
 
 #[inline]
 fn is_locale(locale: &str) -> bool {
@@ -86,7 +94,7 @@ impl I18n {
       }
     }
 
-    let i18n = I18n {
+    let mut i18n = I18n {
       directory: dir.to_string_lossy().replace('\\', "/"),
       locales: options.locales.clone(),
       locale: options.default.unwrap_or(options.locales[0].clone()),
@@ -99,20 +107,63 @@ impl I18n {
         for path in glob::glob(&pattern_path).unwrap().filter_map(std::result::Result::ok) {
           if path.is_file() {
             let full_path = path.to_string_lossy().replace('\\', "/");
-            if let Some(caps) = LOCALE_RE.captures(&full_path) {
-              let locale = caps.get(0).unwrap().as_str();
-              // if locales is empty, ignore and load all
-              if !i18n.locales.contains(&locale.to_string()) {
-                continue;
-              }
-              // TODO: implement
-              println!("{locale}: {full_path}");
+
+            let Some(locale) = LOCALE_RE
+              .captures(&full_path)
+              .and_then(|c| c.get(0))
+              .map(|s| s.as_str())
+            else {
+              // debug
+              continue;
+            };
+            if i18n.locales.contains(&locale.to_string()) {
+              _ = i18n.get_with_path(locale, &full_path)?;
             }
           }
         }
       }
     }
-
     Ok(i18n)
+  }
+
+  /// -- Internal methods --
+
+  #[inline]
+  fn cache(&self) -> Result<MutexGuard<Translations>> {
+    CACHE.lock().map_err(|err| {
+      Error::new(
+        Status::Unknown,
+        format!("Unable to access to global cache: \"{}\"", err),
+      )
+    })
+  }
+
+  #[inline]
+  fn get(&mut self, locale: &str, file: &str) -> Result<TObject> {
+    let file_path = format!("{}/{}/{}", self.directory, locale, file);
+    self.get_with_path(locale, &file_path)
+  }
+
+  fn get_with_path(&mut self, locale: &str, file_path: &str) -> Result<TObject> {
+    let mut cache = self.cache()?;
+    if let Some(cache_obj) = cache.get(locale).and_then(|t| t.get(file_path)) {
+      return Ok(cache_obj.clone());
+    }
+
+    let Some(caps) = FILENAME_RE.captures(file_path) else {
+      return Err(Error::new(
+        Status::Unknown,
+        format!("Unable to parse filename \"{}\"", file_path),
+      ));
+    };
+    let name = caps.get(1).unwrap().as_str();
+
+    let table = parse(file_path)?;
+    let _ = cache
+      .entry(locale.to_string())
+      .or_default()
+      .insert(name.to_string(), table.clone());
+
+    Ok(table)
   }
 }
